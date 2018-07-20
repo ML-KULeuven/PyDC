9c990e9c6a99aa7fb5ce384864ca05136aa6531f
pedrozudo <pedrozuidbergdosmartires@gmail.com>
2018-07-20 13:14:38 +0200

    minor change

--- examples/example_ddc_hmm/example_ddc_hmm.py
+++ examples/example_ddc_hmm/example_ddc_hmm.py
@@ -2,7 +2,8 @@ from pydc import DDC
 
 def main():
     ddc = DDC("example_ddc_hmm.pl", 500)
-    prob_s0 = ddc.query("current(weather(brussels))~=sunny")
+    prob_s0 = ddc.query(
+        "current(weather(brussels))~=sunny")
 
     ddc.step(observations="observation(activity(brussels)~=clean")
     prob_s1 = ddc.query("(current(temperature(brussels))~=X, X>20)")
