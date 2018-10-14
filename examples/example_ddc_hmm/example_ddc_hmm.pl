2cd959cc1ccbd0f86328763c1400c8b2ecf6d592
pedrozudo <pedrozuidbergdosmartires@gmail.com>
2018-10-15 00:25:52 +0200

    smal formatting change

--- examples/example_ddc_hmm/example_ddc_hmm.pl
+++ examples/example_ddc_hmm/example_ddc_hmm.pl
@@ -18,11 +18,10 @@ temperature(C):t ~ gaussian(24,8) <- weather(C):t ~= sunny.
 weather(C):t+1 ~ finite([0.7:rainy,0.3:sunny]) <- weather(C):t ~= rainy.
 weather(C):t+1 ~ finite([0.4:rainy,0.6:sunny]) <- weather(C):t ~= sunny.
 
-activity(tintin):t+1 ~ finite([0.1:walk,0.4:shop,0.5:clean]) <- weather(brussels):t+1 ~= rainy.
-activity(tintin):t+1 ~ finite([0.6:walk,0.3:shop,0.1:clean]) <- weather(brussels):t+1 ~= sunny.
-
-
-
+activity(tintin):t+1 ~finite([0.1:walk,0.4:shop,0.5:clean]) <-
+   weather(brussels):t+1 ~= rainy.
+activity(tintin):t+1 ~ finite([0.6:walk,0.3:shop,0.1:clean]) <-
+   weather(brussels):t+1 ~= sunny.
 
 %observation model add uncertainty if you want
 observation(activity(tintin)):t+1 ~ val(_).
